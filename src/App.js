fe4e23a9cfcfa5523819cb477815e96ff701bedb
Cibiyanna <pkcibiyanna@gmail.com>
2024-04-18 15:06:29 +0530

    clearing bugs

--- src/App.js
+++ src/App.js
@@ -3,17 +3,11 @@ import React , {useEffect} from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Screen1 from './pages/Screen1';
 import Screen2 from './pages/Screen2';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
 import { Toaster } from 'react-hot-toast';
 function App() {
-  useEffect(() => {
-    AOS.init({
-      duration: 2000
-    });
-  }, [])
+  
   return (
-    <div className="App abel-regular w-[100vw] scroll-smooth overflow-hidden" data-aos="fade-up">
+    <div className="App abel-regular w-[100vw] h-[100vh] scroll-smooth overflow-hidden">
       <Toaster/>
         <Router>
           <Routes>
